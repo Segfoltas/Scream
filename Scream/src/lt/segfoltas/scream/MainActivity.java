f23e3c0606e88c615187dd8dad337a444ef0984c
DariusL <d.lapunas@gmail.com>
2014-09-02 14:52:52 +0300

    moving

---
+++ Scream/src/lt/segfoltas/scream/MainActivity.java
@@ -0,0 +1,94 @@
+package lt.segfoltas.scream;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningServiceInfo;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+public class MainActivity extends Activity{
+	
+	private ImageView pebbleScreen;
+	private Handler handler = new Handler();
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		pebbleScreen = (ImageView) findViewById(R.id.pebble_screen);
+		ToggleButton button = (ToggleButton) findViewById(R.id.service_switch);
+		button.setOnCheckedChangeListener(listener);
+		button.setChecked(isServiceRunning(ScreamService.class));
+		registerReceiver(receiver, new IntentFilter(ScreamService.INTENT));
+		startService(new Intent(this, ScreamService.class));
+	}
+	
+	private BroadcastReceiver receiver = new BroadcastReceiver() {
+		
+		@Override
+		public void onReceive(Context context, final Intent intent) {
+			handler.post(new Runnable() {
+				
+				@Override
+				public void run() {
+					if(intent.getAction().equals(ScreamService.INTENT)){
+						if(intent.getBooleanExtra(ScreamService.VALUE, false)){
+							pebbleScreen.setImageResource(R.drawable.screen_on);
+						}else{
+							pebbleScreen.setImageResource(R.drawable.screen_off);
+						}
+					}
+				}
+			});
+			
+		}
+	};
+	
+	@Override
+	protected void onStart() {
+		super.onStart();
+	}
+	
+	@Override
+	protected void onStop() {
+		super.onStop();
+	}
+	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		unregisterReceiver(receiver);
+		Log.d("MainActivity", "destroyed");
+	}
+	
+	private OnCheckedChangeListener listener = new OnCheckedChangeListener() {
+		
+		@Override
+		public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+			if(isChecked)
+				startService(new Intent(MainActivity.this, ScreamService.class));
+			else
+				stopService(new Intent(MainActivity.this, ScreamService.class));
+		}
+	};
+	
+	private boolean isServiceRunning(Class<?> serviceClass) {
+	    ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+	    for (RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+	        if (serviceClass.getName().equals(service.service.getClassName())) {
+	            return true;
+	        }
+	    }
+	    return false;
+	}
+}
---
+++ Scream/src/lt/segfoltas/scream/PebbleInterface.java
@@ -0,0 +1,116 @@
+package lt.segfoltas.scream;
+
+import java.util.UUID;
+
+import com.getpebble.android.kit.PebbleKit;
+import com.getpebble.android.kit.PebbleKit.PebbleDataReceiver;
+import com.getpebble.android.kit.util.PebbleDictionary;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+
+public class PebbleInterface {
+
+	private WearableCallbacks callbacks;
+	private Handler handler;
+	private long timeout;
+	private PebbleDataReceiver receiver;
+	private Context context;
+	private UUID uuid;
+
+	public PebbleInterface(final Context context, final WearableCallbacks callbacks, final UUID uuid, final long timeout) {
+		this.callbacks = callbacks;
+		this.context = context;
+		this.handler = new Handler();
+		this.timeout = timeout;
+		this.uuid = uuid;
+		connect();
+	}
+	
+	public void connect(){
+		if(PebbleKit.isWatchConnected(context))
+			connectedReceiver.onReceive(null, null);
+
+		PebbleKit.registerPebbleConnectedReceiver(context, connectedReceiver);
+
+		PebbleKit.registerPebbleDisconnectedReceiver(context, disconnectedReceiver);
+
+		receiver = new PebbleDataReceiver(uuid) {
+
+			@Override
+			public void receiveData(Context context, int transactionId, PebbleDictionary data) {
+				final long shake = data.getUnsignedInteger(1);
+				updateTimer();
+				handler.post(new ShakeRunnable((int) shake));
+				PebbleKit.sendAckToPebble(context, transactionId);
+			}
+		};
+
+		PebbleKit.registerReceivedDataHandler(context, receiver);
+	}
+	
+	public void disconnect(){
+		context.unregisterReceiver(receiver);
+		context.unregisterReceiver(connectedReceiver);
+		context.unregisterReceiver(disconnectedReceiver);
+		handler.removeCallbacks(timer);
+	}
+
+	private class ShakeRunnable implements Runnable {
+		private final int shake;
+
+		public ShakeRunnable(final int shake) {
+			this.shake = shake;
+		}
+
+		@Override
+		public void run() {
+			callbacks.onShakeReceived(shake);
+		}
+
+	}
+
+	private BroadcastReceiver connectedReceiver = new BroadcastReceiver() {
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			updateTimer();
+			handler.post(new Runnable() {
+
+				@Override
+				public void run() {
+					callbacks.onDeviceConnected();
+				}
+			});
+		}
+	};
+
+	private BroadcastReceiver disconnectedReceiver = new BroadcastReceiver() {
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			handler.post(new Runnable() {
+
+				@Override
+				public void run() {
+					callbacks.onDeviceDisconnected();
+				}
+			});
+		}
+	};
+
+	private void updateTimer() {
+		handler.removeCallbacks(timer);
+		handler.postDelayed(timer, timeout);
+	}
+
+	private Runnable timer = new Runnable() {
+
+		@Override
+		public void run() {
+			callbacks.onTimeout();
+		}
+	};
+}
